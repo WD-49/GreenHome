8003e6f89628b89150eca9bc4a50bc7068930dc0
linhph50911 <linhpnph50911@gmail.com>
2025-05-15 11:20:45 +0700

    migration + model + cat giao dien

---
+++ database/migrations/2025_05_15_020556_create_discounts_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('code', 50)->unique();
+            $table->enum('discount_type', ['percentage', 'fixed']);
+            $table->decimal('discount_value', 10, 2);
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->decimal('max_discount', 10, 2);
+            $table->decimal('min_order_value', 10, 2);
+            $table->unsignedInteger('quantity');
+            $table->unsignedInteger('user_usage_limit');
+            $table->boolean('applies_to_all_products')->default(true);
+            $table->enum('status', ['active', 'inactive', 'expired'])->default('inactive');
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.admin')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }}</title>
-</head>
 
-<body>
+@section('content')
     <h1>{{ $title }}</h1>
-</body>
-
-</html>
+@endsection
