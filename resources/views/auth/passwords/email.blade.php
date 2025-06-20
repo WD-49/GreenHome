8ab84e0c9d7ba754f9a50a50372e879a0e74fd51
Ngọc Dũng <dungpnph52047@gmail.com>
2025-06-20 15:40:25 +0700

    NgocDung Fix Interface ResetsPasswords

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -43,5 +43,270 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+
+
+    <!-- Breadcrumb -->
+    <section class="section-breadcrumb">
+        <div class="cr-breadcrumb-image">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="cr-breadcrumb-title">
+                            <h2>Khôi phục mật khẩu</h2>
+                            <span> <a href="#">Home</a> - Khôi phục mật khẩu</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Forgot page -->
+    <section class="section-login padding-tb-100">
+        <div class="container">
+            <div class="row d-none">
+                <div class="col-lg-12">
+                    <div class="mb-30" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="400">
+                        <div class="cr-banner">
+                            <h2>Forgot Password</h2>
+                        </div>
+                        <div class="cr-banner-sub-title">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut labore lacus vel facilisis. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="cr-login" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="400">
+                        <div class="form-logo">
+                            <img src="{{ asset('assets_client/assets/img/logo/logo.png') }}" alt="logo">
+                        </div>
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <form class="cr-content-form" method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                            <div class="form-group">
+                                <label>Email*</label>
+                                <input id="email" type="email" placeholder="Nhập email của bạn" class="cr-form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="login-buttons">
+                                <button type="submit" class="cr-button">Gửi</button>
+                                <a href="{{ route('register') }}" class="link">
+                                    Bạn chưa có tài khoản?
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    @include('client.partials.sideTool')
+
+    <!-- Tab to top -->
+    <a href="#Top" class="back-to-top result-placeholder">
+        <i class="ri-arrow-up-line"></i>
+        <div class="back-to-top-wrap">
+            <svg viewBox="-1 -1 102 102">
+                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+            </svg>
+        </div>
+    </a>
+
+    <!-- Cart -->
+    <div class="cr-cart-overlay"></div>
+    <div class="cr-cart-view">
+        <div class="cr-cart-inner">
+            <div class="cr-cart-top">
+                <div class="cr-cart-title">
+                    <h6>My Cart</h6>
+                    <button type="button" class="close-cart">×</button>
+                </div>
+                <ul class="crcart-pro-items">
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/4.jpg"
+                                alt="product-1"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Fresh Pomegranate</a>
+                            <span class="cart-price"><span>$56.00</span> x 1kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/2.jpg"
+                                alt="product-2"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Green Apples</a>
+                            <span class="cart-price"><span>$75.00</span> x 1kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/3.jpg"
+                                alt="product-3"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Watermelon - Small</a>
+                            <span class="cart-price"><span>$48.00</span> x 5kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="cr-cart-bottom">
+                <div class="cart-sub-total">
+                    <table class="table cart-table">
+                        <tbody>
+                            <tr>
+                                <td class="text-left">Sub-Total :</td>
+                                <td class="text-right">$300.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left">VAT (20%) :</td>
+                                <td class="text-right">$60.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left">Total :</td>
+                                <td class="text-right primary-color">$360.00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="cart_btn">
+                    <a href="cart.html" class="cr-button">View Cart</a>
+                    <a href="checkout.html" class="cr-btn-secondary">Checkout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Side-tool -->
+    <div class="cr-tool-overlay"></div>
+    <div class="cr-tool">
+        <div class="cr-tool-btn">
+            <a href="javascript:void(0)" class="btn-cr-tool result-placeholder">
+                <i class="ri-settings-line"></i>
+            </a>
+            <div class="color-variant">
+                <div class="cr-bar-title">
+                    <h6>Tools</h6>
+                    <a href="javascript:void(0)" class="close-tools">
+                        <i class="ri-close-line"></i>
+                    </a>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Select Color</h2>
+                    </div>
+                    <ul class="cr-color">
+                        <li class="colors c1 active-colors">
+                        </li>
+                        <li class="colors c2">
+                        </li>
+                        <li class="colors c3">
+                        </li>
+                        <li class="colors c4">
+                        </li>
+                        <li class="colors c5">
+                        </li>
+                        <li class="colors c6">
+                        </li>
+                        <li class="colors c7">
+                        </li>
+                        <li class="colors c8">
+                        </li>
+                        <li class="colors c9">
+                        </li>
+                        <li class="colors c10">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Dark mode</h2>
+                    </div>
+                    <ul class="dark-mode">
+                        <li class="dark">
+                        </li>
+                        <li class="white active-dark-mode">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>RTL mode</h2>
+                    </div>
+                    <ul class="rtl-mode">
+                        <li class="rtl">
+                            <img src="assets/img/tool/rtl.png" alt="rtl">
+                        </li>
+                        <li class="ltr active-rtl-mode">
+                            <img src="assets/img/tool/ltr.png" alt="ltr">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Backgrounds</h2>
+                    </div>
+                    <ul class="bg-panel">
+                        <li class="bg-1">
+                            <img src="assets/img/shape/bg-shape-1.png" alt="bg-shape-1">
+                        </li>
+                        <li class="bg-2">
+                            <img src="assets/img/shape/bg-shape-2.png" alt="bg-shape-2">
+                        </li>
+                        <li class="bg-3">
+                            <img src="assets/img/shape/bg-shape-3.png" alt="bg-shape-3">
+                        </li>
+                        <li class="bg-4">
+                            <img src="assets/img/shape/bg-shape-4.png" alt="bg-shape-4">
+                        </li>
+                        <li class="bg-5">
+                            <img src="assets/img/shape/bg-shape-5.png" alt="bg-shape-5">
+                        </li>
+                        <li class="bg-6 active-bg-panel">
+                            <img src="assets/img/shape/bg-shape-6.png" alt="bg-shape-6">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
