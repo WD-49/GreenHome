63a30551e35f35833e783203d793a36224e2b2dd
Ngọc Dũng <dungpnph52047@gmail.com>
2025-06-20 15:41:09 +0700

    NgocDung Update Logic Send Mail To ResetsPasswords

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
@@ -26,4 +31,34 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
+
+    protected function resetPassword(User $user, $password)
+    {
+        $user->password = Hash::make($password);
+        $user->setRememberToken(Str::random(60));
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        // Gửi mail đơn giản qua hàng đợi, không tạo file riêng
+        Mail::to($user->email)->queue(new class($user, $password) extends Mailable implements ShouldQueue {
+            use Queueable, SerializesModels;
+
+            public $user, $password;
+
+            public function __construct($user, $password)
+            {
+                $this->user = $user;
+                $this->password = $password;
+            }
+
+            public function build()
+            {
+                return $this->subject('Mật khẩu mới từ GreenHome')
+                            ->text('emails.password_plain'); // dùng file blade text
+            }
+        });
+
+        $this->guard()->login($user);
+    }
 }
