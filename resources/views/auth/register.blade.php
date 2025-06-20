f1d2606806921e5fea568f7f7845cd4a2bbb711d
Ngọc Dũng <dungpnph52047@gmail.com>
2025-06-20 14:38:59 +0700

    NgocDung Fix Interface Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,317 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+    @if (session('warning'))
+        <div class="alert alert-warning">
+            {{ session('warning') }}
+        </div>
+    @endif
+   
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    <!-- Breadcrumb -->
+    <section class="section-breadcrumb">
+        <div class="cr-breadcrumb-image">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="cr-breadcrumb-title">
+                            <h2>Đăng ký</h2>
+                            <span> <a href="index.html">Home</a> - Đăng ký</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <!-- Register -->
+    <section class="section-register padding-tb-100">
+        <div class="container">
+            <div class="row d-none">
+                <div class="col-lg-12">
+                    <div class="mb-30" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="400">
+                        <div class="cr-banner">
+                            <h2>Register</h2>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="cr-register" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="400">
+                        <div class="form-logo">
+                            <img src="{{ asset('assets_client/assets/img/logo/logo.png') }}" alt="">
                         </div>
+                        <form class="cr-content-form" method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Họ và tên*</label>
+                                        <input id="name" type="text" placeholder="Enter Your First Name"
+                                            class="cr-form-control @error('name') is-invalid @enderror" name="name"
+                                            value="{{ old('name') }}"  autocomplete="name" autofocus>
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <div class="col-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Email*</label>
+                                        <input id="email" type="email" placeholder="Enter Your email"
+                                            class="cr-form-control @error('email') is-invalid @enderror" name="email"
+                                            value="{{ old('email') }}"  autocomplete="email">
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Mật khẩu*</label>
+                                        <input id="password" type="password" placeholder="Nhập nhập mật khẩu" class="cr-form-control @error('password') is-invalid @enderror" name="password"
+                                             autocomplete="new-password">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                
+                                <div class="col-12 col-sm-12">
+                                    <div class="form-group">
+                                        <label>Xác nhận mật khẩu*</label>
+                                        <input id="password-confirm" type="password" placeholder="Nhập lại mật khẩu" class="cr-form-control" name="password_confirmation"  autocomplete="new-password">
+                                    </div>
+                                </div>
+                                <div class="cr-register-buttons">
+                                    <button type="submit" class="cr-button">Đăng ký</button>
+                                    <a href="{{ route('login') }}" class="link">
+                                        Bạn đã có tài khoản?
+                                    </a>
+                                </div>
                             </div>
-                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+    <!-- Footer -->
+    @include('client.partials.sideTool')
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+    <!-- Tab to top -->
+    <a href="#Top" class="back-to-top result-placeholder">
+        <i class="ri-arrow-up-line"></i>
+        <div class="back-to-top-wrap">
+            <svg viewBox="-1 -1 102 102">
+                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+            </svg>
+        </div>
+    </a>
+
+    <!-- Cart -->
+    <div class="cr-cart-overlay"></div>
+    <div class="cr-cart-view">
+        <div class="cr-cart-inner">
+            <div class="cr-cart-top">
+                <div class="cr-cart-title">
+                    <h6>My Cart</h6>
+                    <button type="button" class="close-cart">×</button>
+                </div>
+                <ul class="crcart-pro-items">
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/4.jpg"
+                                alt="product-1"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Fresh Pomegranate</a>
+                            <span class="cart-price"><span>$56.00</span> x 1kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
                             </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/2.jpg"
+                                alt="product-2"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Green Apples</a>
+                            <span class="cart-price"><span>$75.00</span> x 1kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
                             </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
                         </div>
-                    </form>
+                    </li>
+                    <li>
+                        <a href="product-left-sidebar.html" class="crside_pro_img"><img src="assets/img/product/3.jpg"
+                                alt="product-3"></a>
+                        <div class="cr-pro-content">
+                            <a href="product-left-sidebar.html" class="cart_pro_title">Watermelon - Small</a>
+                            <span class="cart-price"><span>$48.00</span> x 5kg</span>
+                            <div class="cr-cart-qty">
+                                <div class="cart-qty-plus-minus">
+                                    <button type="button" class="plus">+</button>
+                                    <input type="text" placeholder="." value="1" minlength="1" maxlength="20"
+                                        class="quantity">
+                                    <button type="button" class="minus">-</button>
+                                </div>
+                            </div>
+                            <a href="javascript:void(0)" class="remove">×</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="cr-cart-bottom">
+                <div class="cart-sub-total">
+                    <table class="table cart-table">
+                        <tbody>
+                            <tr>
+                                <td class="text-left">Sub-Total :</td>
+                                <td class="text-right">$300.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left">VAT (20%) :</td>
+                                <td class="text-right">$60.00</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left">Total :</td>
+                                <td class="text-right primary-color">$360.00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="cart_btn">
+                    <a href="cart.html" class="cr-button">View Cart</a>
+                    <a href="checkout.html" class="cr-btn-secondary">Checkout</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <!-- Side-tool -->
+    <div class="cr-tool-overlay"></div>
+    <div class="cr-tool">
+        <div class="cr-tool-btn">
+            <a href="javascript:void(0)" class="btn-cr-tool result-placeholder">
+                <i class="ri-settings-line"></i>
+            </a>
+            <div class="color-variant">
+                <div class="cr-bar-title">
+                    <h6>Tools</h6>
+                    <a href="javascript:void(0)" class="close-tools">
+                        <i class="ri-close-line"></i>
+                    </a>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Select Color</h2>
+                    </div>
+                    <ul class="cr-color">
+                        <li class="colors c1 active-colors">
+                        </li>
+                        <li class="colors c2">
+                        </li>
+                        <li class="colors c3">
+                        </li>
+                        <li class="colors c4">
+                        </li>
+                        <li class="colors c5">
+                        </li>
+                        <li class="colors c6">
+                        </li>
+                        <li class="colors c7">
+                        </li>
+                        <li class="colors c8">
+                        </li>
+                        <li class="colors c9">
+                        </li>
+                        <li class="colors c10">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Dark mode</h2>
+                    </div>
+                    <ul class="dark-mode">
+                        <li class="dark">
+                        </li>
+                        <li class="white active-dark-mode">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>RTL mode</h2>
+                    </div>
+                    <ul class="rtl-mode">
+                        <li class="rtl">
+                            <img src="assets/img/tool/rtl.png" alt="rtl">
+                        </li>
+                        <li class="ltr active-rtl-mode">
+                            <img src="assets/img/tool/ltr.png" alt="ltr">
+                        </li>
+                    </ul>
+                </div>
+                <div class="cr-tools-detail">
+                    <div class="heading">
+                        <h2>Backgrounds</h2>
+                    </div>
+                    <ul class="bg-panel">
+                        <li class="bg-1">
+                            <img src="assets/img/shape/bg-shape-1.png" alt="bg-shape-1">
+                        </li>
+                        <li class="bg-2">
+                            <img src="assets/img/shape/bg-shape-2.png" alt="bg-shape-2">
+                        </li>
+                        <li class="bg-3">
+                            <img src="assets/img/shape/bg-shape-3.png" alt="bg-shape-3">
+                        </li>
+                        <li class="bg-4">
+                            <img src="assets/img/shape/bg-shape-4.png" alt="bg-shape-4">
+                        </li>
+                        <li class="bg-5">
+                            <img src="assets/img/shape/bg-shape-5.png" alt="bg-shape-5">
+                        </li>
+                        <li class="bg-6 active-bg-panel">
+                            <img src="assets/img/shape/bg-shape-6.png" alt="bg-shape-6">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
