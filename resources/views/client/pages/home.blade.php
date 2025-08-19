488d4cb33e08c46cb9fd4ff0ef78c1ef52095159
Phùng Ngọc Dũng <dungpnph52047@gmail.com>
2025-08-19 22:15:42 +0700

    update(auth): update Thông báo đăng nhập(Email, Password) thành công trang Users

--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -51,6 +51,61 @@
         $defaultBanner = $banners->where('type', 'slider')->first();
     @endphp
 
+    {{-- Thông báo cho Login Google, Fb --}}
+    <div class="toast-container position-fixed top-0 end-0 p-3">
+        @if (session('success'))
+            <div class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        {{ session('success') }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
+
+        @if (session('error'))
+            <div class="toast align-items-center text-bg-danger border-0" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        {{ session('error') }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
+
+        @if (session('warning'))
+            <div class="toast align-items-center text-bg-warning border-0" role="alert" aria-live="assertive"
+                aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">
+                        {{ session('warning') }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
+    </div>
+    <script>
+        // JS cho phần thông báo Login Google FB
+        document.addEventListener('DOMContentLoaded', function() {
+            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function(toastEl) {
+                return new bootstrap.Toast(toastEl, {
+                    autohide: true,
+                    delay: 5000 // 5 giây
+                })
+            })
+            toastList.forEach(toast => toast.show())
+        });
+    </script>
+
     <!-- Hero slider -->
     <section class="section-hero padding-b-100 next">
         <div class="cr-slider swiper-container">
